e3ae3535d1d28e6f8423326781ba4e7621223df5
Aldmsk <Aldsmsk@gmail.com>
2023-01-14 15:20:59 +0300

    Исправил программу

---
+++ Programm/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = new string[]{"1231", "gdfgdg", "33", "World", "gg"};
+string[] arrayTwo = new string[array.Length];
+int wordSize = 3;
+PoiskSlov(array, arrayTwo);
+PrintArray(arrayTwo);
+void PoiskSlov(string[] array, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= wordSize)
+        {
+            arrayTwo[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] arrayTwo)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(arrayTwo[i] + " ");
+    }
+}
+
